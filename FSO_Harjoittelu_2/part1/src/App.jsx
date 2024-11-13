312dff6016a92e6f7f304030656ecac49f788049
miikkarautio <miikka.rautio01@gmail.com>
2024-11-13 13:52:14 +0200

    Part2 practise. noteService created as a module.

--- FSO_Harjoittelu_2/part1/src/App.jsx
+++ FSO_Harjoittelu_2/part1/src/App.jsx
@@ -28,9 +28,35 @@ const App = (props) => {
       id: String(notes.length + 1),
     }
     
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   
+  }
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
+  const Note = ({note, toggleImportance}) => {
+    const label = note.important
+    ? 'make not important' : 'make important'
+
+    return(
+      <li>
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    )
   }
 
   const handleNoteChange = (event) => {
@@ -54,7 +80,10 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note)=>
-          <Note key={note.id} note={note}/>
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
