ee58c0d815fbdca502cc3e3aaac4877de6bb3efc
Miikka Rautio <randomguy1512@gmail.comm>
2025-02-25 15:20:24 +0200

    Assignment 2.20 ongoing. Currently fetches weather data successfully

--- FSO02/FSO2_maidentiedot/src/App.jsx
+++ FSO02/FSO2_maidentiedot/src/App.jsx
@@ -30,17 +30,35 @@ const CountrySearch = (props) => {
       <p>Find countries <input type="text" id="country" onChange={props.findCountry} /></p>
       {props.errorMessage && <p>{props.errorMessage}</p>}
       {props.foundCountries.map((country) =>
-        <p key={country.cca3}>{country.name.common} <button onClick={() => props.setActiveCountry(country)}>Show</button></p>
+        <p key={country.cca3}>{country.name.common}
+        <button onClick={() => props.setActiveCountry(country)}>
+          Show
+        </button>
+        </p>
       )}
     </>
   )
 }
 
+const WeatherData = (props) => {
+  return(
+    <>
+      <div>
+        <h1>Weather in {props.weatherData.name}</h1>
+      </div>
+    </>
+  )
+}
+
+
 function App() {
-  const [country, setCountry] = useState({})
+  const [country, setCountry] = useState([])
   const [foundCountries, setFoundCountries] = useState([])
   const [errorMessage, setErrorMessage] = useState("")
   const [countryInfo, setCountryInfo] = useState([])
+  const [weatherData, setWeatherData] = useState([])
+  const [longitude, setLongitude] = useState("")
+  const [latitude, setLatitude] = useState("")
 
 
   useEffect(() => {
@@ -48,6 +66,21 @@ function App() {
       .get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then(response => {setCountry(response.data)})
   }, []);
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      console.log("Latitude:", latitude,  "longitude:", longitude);
+    }
+  }, [latitude, longitude]);
+
+  useEffect(() => {
+    if(latitude && longitude){
+      axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API-KEY}`)
+      .then(response => (console.log(response.data)))
+    }
+  }, [longitude, latitude]);
+
 
   const findCountry = (e) => {
     const foundCountry = e.target.value.trim().toLowerCase();
@@ -75,10 +108,13 @@ function App() {
       setCountryInfo([])
     }
 
+
   }
 
   const setActiveCountry = (selectedCountry) => {
     setCountryInfo([selectedCountry])
+    setLatitude(selectedCountry.latlng[0]);
+    setLongitude(selectedCountry.latlng[1]); 
     if(selectedCountry){
       setFoundCountries([])
     }
@@ -92,8 +128,11 @@ function App() {
       errorMessage={errorMessage}
       foundCountries={foundCountries}
       setActiveCountry={setActiveCountry}
+      setLatitude={setLatitude}
+      setLongitude={setLongitude}
       />
-      <CountryInformation countryInfo={countryInfo} />
+      <CountryInformation countryInfo={countryInfo}/>
+      <WeatherData weatherData={weatherData.name}/>
     </>
   )
 }
