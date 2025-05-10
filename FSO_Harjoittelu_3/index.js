0bdfb59d216014ba62962019a09a71f0b73bbbc4
miikkarautio <miikka.rautio01@gmail.com>
2025-05-10 19:39:18 +0300

    Some automation for the Render website launch

--- FSO_Harjoittelu_3/index.js
+++ FSO_Harjoittelu_3/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+
 
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(cors())
 
 
 let notes = [
