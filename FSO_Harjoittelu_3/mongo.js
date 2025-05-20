08ae617000499e282eeea6b898bdcf034b956bdb
miikkarautio <miikka.rautio01@gmail.com>
2025-05-20 15:10:03 +0300

    Practise part3-4

--- FSO_Harjoittelu_3/mongo.js
+++ FSO_Harjoittelu_3/mongo.js
@@ -20,7 +20,7 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({important: true}).then(result => {
+Note.find({}).then(result => {
   result.forEach(note => {
     console.log(note)
   })
